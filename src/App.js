34481cd19a543ab7cb99ae6149b30722143a80d0
Shubham-Anncode <141210009+Shubham-Anncode@users.noreply.github.com>
2023-12-23 23:43:09 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import Home from './Components/Home';
-import Header from './Components/Header';
-// import { ContextProvider } from './Components/Context/Context';
 function App() {
   return (
     
     <div className="App">
-      {/* <Header/> */}
       <Home />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ContextProvider } from './Components/Context/Context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <ContextProvider> */}
       <App />
-    {/* </ContextProvider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
